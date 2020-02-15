797dc5ecd21b851daf5ade8646ba085ead56ec4c
Andre Entrekin <andre.entrekin@gmail.com>
2020-02-15 15:48:21 -0600

    cleaning code

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,48 +1,45 @@
-$(function () {
-    $(".devour-burger").on("click", function (event) {
-        console.log("clicked");
-        var id = $(this).data("id");
-        var eaten = $(this).data("devour");
-        console.log(eaten);
+$(function() {
+  $(".devour-burger").on("click", function(event) {
+    console.log("clicked");
+    var id = $(this).data("id");
+    var eaten = $(this).data("devour");
+    console.log(eaten);
 
+    var burgerDevoured = {
+      devoured: eaten
+    };
+    console.log(burgerDevoured);
 
-        var burgerDevoured = {
-            devoured: eaten
-        };
-        console.log(burgerDevoured);
-
-        // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: burgerDevoured
-        }).then(
-            function () {
-                // console.log("changed sleep to", newState);
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: burgerDevoured
+    }).then(function() {
+      // console.log("changed sleep to", newState);
+      // Reload the page to get the updated list
+      location.reload();
     });
+  });
 
-    $(".create-form").on("submit", function (event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
 
-        var newBurger = {
-            name: $(".BN").val().trim(),
-            devoured: 0
-        };
+    var newBurger = {
+      name: $(".BN")
+        .val()
+        .trim(),
+      devoured: 0
+    };
 
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(
-            function () {
-                console.log("created new burger");
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
     });
+  });
 });
